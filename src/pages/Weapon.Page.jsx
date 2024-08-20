d3e1a276fdccd451dd3fc0f1dff6a1471244b501
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2024-08-20 12:25:01 +0530

    minor changes

--- src/pages/Weapon.Page.jsx
+++ src/pages/Weapon.Page.jsx
@@ -1,22 +1,11 @@
 
-
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react'
 
 import DefaultHOC from '../layout/Default.HOC';
 import WeaponInfoPoster from '../components/Poster/WeaponInfoPoster.Component';
 
 const WeaponInfoPage = () => {
-    // const weaponId = useParams();
-    // const[getWeaponData, setWeaponData] = useState([]);
-    // useEffect(()=>{
-    //     const apiGetWeapon = async ()=>{
-    //         const fetchWeaponData = await axios.get(`/weapons/${weaponId.id}`);
-    //         setWeaponData(fetchWeaponData.data.data);
-    //     }
-    //     apiGetWeapon();
-    // },[weaponId]);
+
     return (
         <div>
             <WeaponInfoPoster />
